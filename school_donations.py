a87286e30c670f39a123cc8f72182ee0834d230c
Mauya <smkadungure@live.co.uk>
2017-05-24 20:22:11 +0100

    Added import os and and set environment variables

--- school_donations.py
+++ school_donations.py
@@ -2,12 +2,13 @@ from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
 import json
+import os
+
 
 app = Flask(__name__)
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-DBS_NAME = 'donosUSA'
+MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
+DBS_NAME = os.getenv('MONGO_DB_NAME', 'donorsUSA')
 COLLECTION_NAME = 'projects'
 
 
